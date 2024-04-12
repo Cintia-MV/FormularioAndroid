f6caeb3abc353c6357b550271622790ee92b2a06
Daniel Avila <daniel.avilaprado@gmail.com>
2024-04-11 22:13:07 -0400

    Agrego comentarios a las activities

--- app/src/main/java/com/example/formulario/MainActivity.java
+++ app/src/main/java/com/example/formulario/MainActivity.java
@@ -15,32 +15,35 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) { // aquí se inicializa la interfaz de usuario y se configuran los componentes de la actividad
+        super.onCreate(savedInstanceState); // Esta línea llama al método onCreate de la clase base, es decir, AppCompatActivity, para que realice cualquier inicialización necesaria.
+        setContentView(R.layout.activity_main); //Infla el diseño definido en el archivo activity_main.xml y lo muestra en la pantalla
 
+        //Creo variables a partir del id del elemento del xml
         EditText nombre = findViewById(R.id.nombre);
         EditText apellido = findViewById(R.id.apellido);
         EditText email = findViewById(R.id.email);
         EditText clave = findViewById(R.id.clave);
         Button btnCrear = findViewById(R.id.btnCrear);
 
+        //Evento clic
         btnCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Capturo los datos ingresados por el usuario
                String nombreIn = nombre.getText().toString();
                String apellidoIn = apellido.getText().toString();
                String emailIn = email.getText().toString();
                String claveIn = clave.getText().toString();
 
+                // Creo un intent para iniciar la actividad MostrarDatosActivity
+                Intent intent = new Intent(MainActivity.this, MostrarDatosActivity.class); // MainActivity.this = hago referencia a la clase actual // MostrarDatosActivity.class  Esta es la clase que quiero "redireccionar"
 
-                Intent intent = new Intent(MainActivity.this, MostrarDatosActivity.class);
-
-                intent.putExtra("nombre", nombreIn);
+                intent.putExtra("nombre", nombreIn); // nombre, nombreIn = clave:valor   agrego datos adicionales al intent
                 intent.putExtra("apellido", apellidoIn);
                 intent.putExtra("email", emailIn);
                 intent.putExtra("clave", claveIn);
-                startActivity(intent);
+                startActivity(intent); // Inicia la actividad MostrarDatosActivity
 
             }
         });
--- app/src/main/java/com/example/formulario/MostrarDatosActivity.java
+++ app/src/main/java/com/example/formulario/MostrarDatosActivity.java
@@ -2,6 +2,8 @@ package com.example.formulario;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,18 @@ public class MostrarDatosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_datos);
 
-        Intent intent = getIntent();
-        String nombre = intent.getStringExtra("nombre");
+        // Obtiene el intent que inició esta actividad.
+        Intent intent = getIntent(); //con esto obtengo los datos ingresados en la pantalla anterior
+
+        //Creo "variables" a traves del intent con los datos ingresados
+        String nombre = intent.getStringExtra("nombre");  //Extraigo el valor asociado con la clave "nombre" del intento.
         String apellido = intent.getStringExtra("apellido");
         String email = intent.getStringExtra("email");
         String clave = intent.getStringExtra("clave");
+
+        //Muestro un mensaje en el textview de bienvenida
+        TextView bienvenida = findViewById(R.id.bienvenida);
+        bienvenida.setText("Bienvenido " +nombre +" "+apellido);
 
         // Muestra los datos en los TextView correspondientes
         TextView nombreIngresado = findViewById(R.id.nombreIngresado);
@@ -25,11 +34,25 @@ public class MostrarDatosActivity extends AppCompatActivity {
         TextView apellidoIngresado = findViewById(R.id.apellidoIngresado);
         apellidoIngresado.setText("Apellido: " +apellido);
 
-
         TextView correoIngresado = findViewById(R.id.correoIngresado);
         correoIngresado.setText("Correo: " +email);
 
         TextView claveIngresada = findViewById(R.id.claveIngresada);
         claveIngresada.setText("Contraseña: " +clave);
+
+        //Declaro el boton para volver a la pantalla anterior
+        Button btnVolver = findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MostrarDatosActivity.this, MainActivity.class);
+                startActivity(intent); //Vuelvo a la pantalla anterior
+            }
+        });
+
+
+
+
     }
 }
